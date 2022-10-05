5f0842167e989760343c8a2a85aab33ab788816c
MOPSKATER <josua.w@gmx.de>
2022-10-05 14:03:17 +0200

    Load/Save

--- VaniTAS/TASManager.cs
+++ VaniTAS/TASManager.cs
@@ -1,4 +1,7 @@
-﻿namespace VaniTAS
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace VaniTAS
 {
     internal static class TASManager
     {
@@ -9,13 +12,51 @@
         {
             _levelID = levelID;
             _currentTAS = actions;
+            SaveToFile();
         }
 
         public static List<InputAction> GetTAS(string levelID)
         {
             if (levelID == _levelID) return _currentTAS;
+            LoadFromFile(levelID);
+            return _currentTAS;
+        }
 
-            return null; // TODO load TAS
+        private static void SaveToFile()
+        {
+            string path = "";
+            if (!GhostUtils.GetPath(Main.Game.GetCurrentLevel().levelID, GhostUtils.GhostType.PersonalGhost, ref path)) return;
+
+            path += "\\Vani.TAS";
+
+            Stream stream = File.Open(path, FileMode.Create);
+            BinaryFormatter formatter = new();
+            formatter.Serialize(stream, _currentTAS);
+            stream.Close();
+        }
+
+        private static void LoadFromFile(string levelID)
+        {
+            _currentTAS = null;
+            string path = "";
+            if (!GhostUtils.GetPath(levelID, GhostUtils.GhostType.PersonalGhost, ref path)) return;
+
+            path += "\\Vani.TAS";
+
+            try
+            {
+                Stream stream = File.Open(path, FileMode.Open);
+                BinaryFormatter formatter = new();
+                _currentTAS = (List<InputAction>)formatter.Deserialize(stream);
+                stream.Close();
+            } catch (FileNotFoundException fileNotFound)
+            {
+                Debug.Log(fileNotFound.Message);
+            } catch (Exception e)
+            {
+                Debug.LogException(e);
+                Debug.LogError("Following TAS file might be broken: " + path);
+            }
         }
     }
 }
